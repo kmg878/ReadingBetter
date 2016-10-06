900449ae3d982b67f6ed6d5ece153bb088d31e86
MinjeongKim <kmg878@naver.com>
2016-10-06 15:33:28 +0900

    컨트롤러 app 랭킹 책 마이페이지 퀴즈

--- src/main/java/kr/ac/readingbetter/controller/app/AppBookController.java
+++ src/main/java/kr/ac/readingbetter/controller/app/AppBookController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.ac.readingbetter.service.BookService;
 import kr.ac.readingbetter.service.QuizService;
+import kr.ac.readingbetter.service.ReviewService;
 import kr.ac.readingbetter.vo.BookVo;
 import kr.ac.readingbetter.vo.QuizVo;
+import kr.ac.readingbetter.vo.ReviewVo;
 
 
 @Controller
@@ -22,8 +24,10 @@ public class AppBookController {
 	@Autowired
 	private BookService bookService;
 	
+	
+	
 	@Autowired
-	private QuizService quizService;
+	private ReviewService reviewService;
 	
 	@ResponseBody
 	@RequestMapping(value = "list", method = RequestMethod.GET)
@@ -32,23 +36,28 @@ public class AppBookController {
 		
 		return list;
 	}
+
 	
 	@ResponseBody
-	@RequestMapping(value = "insertquiz", method = RequestMethod.GET)
-	public String InsertQuiz(@ModelAttribute QuizVo vo,
-			Long bookNo,String quiz,String ex1,String ex2,String ex3,String ex4,String answer ) {
-		System.out.println(bookNo+","+quiz+","+ex1+","+ex2+","+ex3+","+ex4+","+answer);
+	@RequestMapping(value = "review", method = RequestMethod.GET)
+	public List<ReviewVo> Review(Long bookNo,ReviewVo reviewVo) {
+		reviewVo.setBookNo(bookNo);
+		List<ReviewVo> list = reviewService.getListApp(reviewVo);
+		return list;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "insertreview", method = RequestMethod.GET)
+	public String InsertReview(Long bookNo,String review,Long memberNo,@ModelAttribute ReviewVo vo) {
+		System.out.println(bookNo+","+review+","+memberNo);
 		vo.setBookNo(bookNo);
-		vo.setQuiz(quiz);
-		vo.setEx1(ex1);
-		vo.setEx2(ex2);
-		vo.setEx3(ex3);
-		vo.setEx4(ex4);
-		vo.setAnswer(answer);
-		quizService.quizAddUser(vo);
+		vo.setReview(review);
+		vo.setMemberNo(memberNo);
+		reviewService.insert(vo);
 		
 		return "";
 	}
+	
 	
 	
 
--- src/main/java/kr/ac/readingbetter/controller/app/AppQuizController.java
+++ src/main/java/kr/ac/readingbetter/controller/app/AppQuizController.java
@@ -1,5 +1,37 @@
 package kr.ac.readingbetter.controller.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.ac.readingbetter.service.QuizService;
+import kr.ac.readingbetter.vo.QuizVo;
+
+@Controller
+@RequestMapping("/quizapp")
 public class AppQuizController {
+	
+	@Autowired
+	private QuizService quizService;
+	
+	@ResponseBody
+	@RequestMapping(value = "insertquiz", method = RequestMethod.GET)
+	public String InsertQuiz(@ModelAttribute QuizVo vo,
+			Long bookNo,String quiz,String ex1,String ex2,String ex3,String ex4,String answer ) {
+		System.out.println(bookNo+","+quiz+","+ex1+","+ex2+","+ex3+","+ex4+","+answer);
+		vo.setBookNo(bookNo);
+		vo.setQuiz(quiz);
+		vo.setEx1(ex1);
+		vo.setEx2(ex2);
+		vo.setEx3(ex3);
+		vo.setEx4(ex4);
+		vo.setAnswer(answer);
+		quizService.quizAddUser(vo);
+		
+		return "";
+	}
 
 }
